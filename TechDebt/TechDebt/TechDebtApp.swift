01f75068328162b5a8b36003eaf2eaa8aa1fffea
Amy Roche <141393246+amezzles@users.noreply.github.com>
2025-05-02 14:13:12 +1000

    project setup

---
+++ TechDebt/TechDebt/TechDebtApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechDebtApp.swift
+//  TechDebt
+//
+//  Created by Amy Roche on 2/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct TechDebtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
