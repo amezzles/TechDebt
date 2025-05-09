5fa1a377a4e7a9bcdd5317fe45815691fd68cdef
Resipate <ashtonbarker060@gmail.com>
2025-05-09 14:23:14 +1000

    made app data

---
+++ TechDebt/TechDebt/AppData.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+final class AppDataManager: ObservableObject {
+    static var instance = AppDataManager()
+    public init() { Load() }
+    
+    private let SaveKey = "TechDebt"
+    
+    @Published var data: AppData = AppData()
+    
+    func Save(appData: AppData)
+    {
+        if let encoded = try? JSONEncoder().encode(data) {
+            UserDefaults.standard.set(data, forKey: SaveKey)
+        }
+    }
+    
+    func Load()
+    {
+        if let savedData = UserDefaults.standard.data(forKey: SaveKey),
+           let decoded = try? JSONDecoder().decode(AppData.self, from: savedData) {
+            data = decoded
+        }
+    }
+    
+}
+
+struct AppData : Codable {
+    var budgetAmount = 0
+    var budgetRemaining = 0
+    var budgePeriod = 0
+    var saveAmount = 0
+    var saveGoalText = 0
+    var hasSet = false
+}
