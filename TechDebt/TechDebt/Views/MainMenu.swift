85140df08b58747cac27811965a8733631c21a0f
Oscar <94662482+oc10000@users.noreply.github.com>
2025-05-11 22:12:55 +1000

    Completed pie chart and remaning budg display

--- TechDebt/TechDebt/Views/MainMenu.swift
+++ TechDebt/TechDebt/Views/MainMenu.swift
@@ -1,12 +1,28 @@
 import SwiftUI
+import Charts
 
 struct MainMenu: View {
     @ObservedObject var appManager: AppManager
     @ObservedObject var appData: AppDataManager
     
     var body: some View {
-        ZStack{
+        VStack {
             Text("MainMenu")
+                .padding()
+
+            Chart {
+                ForEach(appData.regularExpenditures) { item in
+                    SectorMark(
+                        angle: .value("Expenditure", item.amount),
+                        name: item.name
+                    )
+                    .foregroundStyle(Color.random) 
+                }
+            }
+            .padding()
+
+            Text("Budget Remaining: \(appData.budgetRemaning, specifier: "%.2f")") //.2f to keep budget remaining as currency
+                .padding()
         }
     }
 }
