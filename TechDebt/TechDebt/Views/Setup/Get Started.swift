5a1d404f25ef2a64f56376ec6ec427f3e8ee6063
Amy Roche <141393246+amezzles@users.noreply.github.com>
2025-05-11 15:33:45 +1000

    startup screen

--- TechDebt/TechDebt/Views/Setup/Get Started.swift
+++ TechDebt/TechDebt/Views/Setup/Get Started.swift
@@ -1,12 +1,1 @@
-import SwiftUI
 
-struct GetStarted: View {
-    @ObservedObject var appManager: AppManager
-    @ObservedObject var appData: AppDataManager
-    
-    var body: some View {
-        ZStack{
-            Text("hi")
-        }
-    }
-}
