5f46858f86c1aaf776d450c2b6bf03738a93def5
Nashuo23 <1chomacalcazaren@gmail.com>
2025-05-11 17:22:23 +1000

    Basic UI elements added
    first commit!

--- TechDebt/TechDebt/Views/Expenditures.swift
+++ TechDebt/TechDebt/Views/Expenditures.swift
@@ -11,9 +11,49 @@ struct AddTransaction: View {
     @ObservedObject var appManager: AppManager
     @ObservedObject var appData: AppDataManager
     
+    @State private var amount: String = ""
+    @State private var category: String = ""
+    
     var body: some View {
         ZStack{
-            Text("hi")
+            Color.white.ignoresSafeArea()
+            VStack(spacing: 30) {
+                Text("New Transaction")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                Text("$")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                TextField("0", text: $amount)
+                    .keyboardType(.decimalPad)
+                Text("Category")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                TextField("", text: $category)
+                    .keyboardType(.default)
+                    .background(.gray.opacity(0.2))
+                Button(action: {
+                    print("Button was tapped!")
+                }) {
+                    Text("Save")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+            }
         }
     }
 }
+
+
+//#Preview {
+//    AddTransaction(appManager: AppManager(), appData: AppDataManager())
+//}
+
+
+
